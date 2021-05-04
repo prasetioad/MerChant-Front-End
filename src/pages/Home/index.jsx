4f500f038f6d8574dc2694631dbdcffda60f31ef
unknown <investor.ciwin@gmail.com>
2021-05-04 13:36:25 +0700

    Boilerplate v3

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet'
 import './style.css'
 // ATOMS
+import { Button } from '../../components/atoms'
 
 export default function Home(){
    return(
@@ -10,6 +11,7 @@ export default function Home(){
          </Helmet>
          <div className="displayColumn inDevelopment">
             <h1>Home - In Development</h1>
+            <Button cls="loginBtn" btnClr="#273AC7" ftClr="white" val="Home"/>
          </div>
       </div>
    )
